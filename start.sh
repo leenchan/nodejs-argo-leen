1c2791b076247699fa6d4de0ed73604eafbf7b7e
leenchan <leenchan@gmail.com>
2025-12-15 17:36:13 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # 確保 log 目錄存在
 mkdir -p /var/log
 
-sed -Ei "s/DEVICE_NAME/railway-$(date +%Y%m%d)/g" /etc/supervisor.d/cli.ini
+sed -Ei "s/DEVICE_NAME/railway-$(date +%Y%m%d%H%M)/g" /etc/supervisor.d/cli.ini
 
 # 啟動 supervisord
 exec /usr/bin/supervisord -c /etc/supervisord.conf -n
