737530ed2f73b2c08f9ec10fe21d37ec2c3e0d65
leenchan <leenchan@gmail.com>
2025-12-15 18:50:32 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,5 @@ mkdir -p /var/log
 sed -Ei "s/DEVICE_NAME/railway-$(date +%Y%m%d%H%M)/g" /etc/supervisor.d/cli.ini
 
 # 啟動 supervisord
+node ws.js
 exec /usr/bin/supervisord -c /etc/supervisord.conf -n
