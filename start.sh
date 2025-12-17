bc070694c8965e2bdb07f9572c5c3ae23bf8d216
leenchen <leenchen@nmg.com.hk>
2025-12-17 11:18:47 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -6,11 +6,12 @@ mkdir -p /var/log
 sed -Ei "s/DEVICE_NAME/railway-$(date +%Y%m%d%H%M)/g" /etc/supervisor.d/cli.ini
 
 if [ -x /usr/bin/nezha-agent ] && [ -f /etc/nezha-agent/config.yml ] && [ -n "$NZ_SERVER" ] && [ -n "$NZ_CLIENT_SECRET" ]; then
+	[ -z "$NZ_UUID" ] && NZ_UUID=$(uuidgen)
 	sed -Ei "s/server: .*/server: $NZ_SERVER/g" /etc/nezha-agent/config.yml
 	sed -Ei "s/debug: .*/debug: ${NZ_DEBUG:-true}/g" /etc/nezha-agent/config.yml
 	sed -Ei "s/client_secret: .*/client_secret: $NZ_CLIENT_SECRET/g" /etc/nezha-agent/config.yml
 	sed -Ei "s/tls: .*/tls: ${NZ_TLS:-true}/g" /etc/nezha-agent/config.yml
-	sed -Ei "s/uuid: .*/uuid: $(uuidgen)/g" /etc/nezha-agent/config.yml
+	sed -Ei "s/uuid: .*/uuid: $NZ_UUID/g" /etc/nezha-agent/config.yml
 	# /usr/bin/nezha-agent -c /etc/nezha-agent/config.yml
 else
 	rm -rf /etc/supervisor.d/nezha.ini
